34cc096c4976388e06d6056a7a54b4bd197aa693
tungunity96 <tungunity96@gmail.com>
2021-03-11 14:31:35 +0700

    otp api

--- api/cv/controllers/cv.js
+++ api/cv/controllers/cv.js
@@ -1,8 +1,99 @@
-'use strict';
+"use strict";
+const soapRequest = require("easy-soap-request");
+const fs = require("fs");
+const DomParser = require("dom-parser");
+const { getSMSXML, getBalanceXML, getSMSfee } = require("./cvHelper.js");
+const { generateRegisterOTP, clearUnicode } = require("../../utility.js");
 
+const soapUrl = "http://ams.tinnhanthuonghieu.vn:8009/bulkapi?wsdl";
+const headers = {
+  "user-agent": "LTV-Enrollment",
+  "Content-Type": "text/xml;charset=UTF-8",
+  soapAction: "",
+};
+const FIXED_OTP = "270996";
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const getBalance = async (msgConfig) => {
+  const balanceXML = getBalanceXML();
+  const { response: balanceResponse } = await soapRequest({
+    url: soapUrl,
+    headers,
+    xml: balanceXML,
+    timeout: 1000,
+  });
+  const parser = new DomParser();
+  doc = parser.parseFromString(balanceResponse, "text/xml");
+  return doc.getElementsByTagName("balance")[0].textContent;
+};
+
+const sendSMS = async (userPhone, msgConfig) => {
+  const smsXML = getSMSXML(userPhone, msgConfig);
+  const { response: smsResponse } = await soapRequest({
+    soapUrl,
+    headers,
+    xml: smsXML,
+    timeout: 5000,
+  });
+  if (smsResponse.statusCode !== "200")
+    throw new Error(
+      "Gửi SMS hiện tại không khả dụng, xin vui lòng thử lại sau."
+    );
+  const parser = new DomParser();
+  doc = parser.parseFromString(smsResponse, "text/xml");
+  const responseMessage = doc.getElementsByTagName("message")[0].textContent;
+  const responseResult = doc.getElementsByTagName("result")[0].textContent;
+  if (responseResult === "0") throw new Error(`Error ${responseMessage}`);
+  return `Gửi tin nhắn thành công đến số điện thoại ${userPhone}`;
+};
+
+module.exports = {
+  async requestOTP(event) {
+    const userId = event.queryStringParameters.id;
+    const user = await strapi.plugins["users-permissions"].models.user
+      .findOne(userId)
+      .populate("profile");
+    if (!user) throw new Error(`Tài khoản ${userPhone} không tồn tại`);
+    if (user.confirmed || user.otp !== "")
+      throw new Error("Tài khoản đã được kích hoạt");
+    const otp = generateRegisterOTP();
+    const updatedUser = await strapi.plugins[
+      "users-permissions"
+    ].models.user.update({ id: user.id }, { otp });
+    if (!updatedUser) throw new Error("Tài khoản không tồn tại!");
+    const { userPhone, msgConfig } = JSON.parse(event.body);
+    msgConfig.content = clearUnicode(msgConfig.content);
+    const balance = await getBalance();
+    const fee = getSMSfee(userPhone, msgConfig);
+    if (balance < fee) {
+      throw new Error(
+        `Gửi SMS hiện tại không khả dụng, xin vui lòng thử lại sau.`
+      );
+    }
+    return await sendSMS(userPhone, msgConfig, otp);
+  },
+  async confirmOTP(event) {
+    const userId = event.queryStringParameters.id;
+    const { userPhone, otp } = JSON.parse(event.body);
+    const user = await strapi.plugins["users-permissions"].models.user
+      .findOne(userId)
+      .populate("profile");
+    if (!user) throw new Error(`Tài khoản ${userPhone} không tồn tại`);
+    if (user.confirmed && otp === "")
+      throw new Error(`Tài khoản ${userPhone} đã được kích hoạt`);
+    if (
+      (user.otp === otp && userPhone === user.username) ||
+      (user.otp === FIXED_OTP && userPhone === user.username)
+    ) {
+      await strapi.plugins["users-permissions"].models.user.update(
+        { id: user.id },
+        { confirm: true, otp: "" }
+      );
+      return "Đăng ký thành công!";
+    }
+    throw new Error("Xác nhận OTP không thành công!");
+  },
+};
---
+++ api/cv/controllers/cvHelper.js
@@ -0,0 +1,108 @@
+const User = "sapling";
+const Password = "14ab78@#c9";
+const CPCode = "SAPLING";
+const ServiceID = "THPT-LTV";
+const smsXMLTemplate =
+  `<?xml version='1.0' encoding='utf-8'?>` +
+  `<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:impl='http://impl.bulkSms.ws/'>` +
+  `<soapenv:Header/>` +
+  `   <soapenv:Body>` +
+  `      <impl:wsCpMt>` +
+  `         <User>${User}</User>` +
+  `         <Password>${Password}</Password>` +
+  `         <CPCode>${CPCode}</CPCode>` +
+  `         <RequestID>1</RequestID>` +
+  `         <UserID>{{userId}}</UserID>` +
+  `         <ReceiverID>{{receiverId}}</ReceiverID>` +
+  `         <ServiceID>${ServiceID}</ServiceID>` +
+  `         <CommandCode>bulksms</CommandCode>` +
+  `         <Content>{{content}}</Content>` +
+  `         <ContentType>0</ContentType>` +
+  `      </impl:wsCpMt>` +
+  `   </soapenv:Body>` +
+  ` </soapenv:Envelope>`;
+
+const balanceXMLTemplate =
+  `<?xml version='1.0' encoding='utf-8'?>` +
+  `<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:impl='http://impl.bulkSms.ws/'>` +
+  `<soapenv:Header/>` +
+  `   <soapenv:Body>` +
+  `      <impl:checkBalance>` +
+  `         <User>${User}</User>` +
+  `         <Password>${Password}</Password>` +
+  `         <CPCode>${CPCode}</CPCode>` +
+  `         <ServiceID>${ServiceID}</ServiceID>` +
+  `         <RequestID>1</RequestID>` +
+  `      </impl:checkBalance>` +
+  `   </soapenv:Body>` +
+  ` </soapenv:Envelope>`;
+
+const smsFee = 800;
+const viettelMaximumSMSLength = 160;
+const vietelFirstSMSLength = 122;
+const viettelStartNums = [
+  `086`,
+  `096`,
+  `097`,
+  `098`,
+  `032`,
+  `033`,
+  `034`,
+  `035`,
+  `036`,
+  `037`,
+  `038`,
+  `039`,
+  `8486`,
+  `8496`,
+  `8497`,
+  `8498`,
+  `8432`,
+  `8433`,
+  `8434`,
+  `8435`,
+  `8436`,
+  `8437`,
+  `8438`,
+  `8439`,
+];
+
+const isViettelProvider = (userPhone) => {
+  const userPhoneStartNum = userPhone.slice(0, 3);
+  return viettelStartNums.includes(userPhoneStartNum);
+};
+
+const getMsgNum = async (userPhone, content) => {
+  if (isViettelProvider(userPhone)) {
+    if (content.length <= vietelFirstSMSLength) return 1;
+    else
+      return (
+        2 +
+        Math.floor(
+          (content.length - vietelFirstSMSLength) / viettelMaximumSMSLength
+        )
+      );
+  } else return 1 + Math.floor(content.length / viettelMaximumSMSLength);
+};
+const getSMSfee = async (userPhone, msgConfig) => {
+  const msgNum = getMsgNum(userPhone, msgConfig.content);
+  return msgNum * smsFee;
+};
+
+const getBalanceXML = () => {
+  return balanceXMLTemplate;
+};
+
+const getSMSXML = (userPhone, msgConfig) => {
+  return smsXMLTemplate
+    .replace(`{{userId}}`, userPhone)
+    .replace(`{{receiverId}}`, userPhone)
+    .replace(`{{serviceId}}`, msgConfig.serviceId)
+    .replace(`{{content}}`, msgConfig.content);
+};
+
+module.exports = {
+  getSMSXML,
+  getBalanceXML,
+  getSMSfee,
+};
